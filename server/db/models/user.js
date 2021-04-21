544bdf1bf412e3536475101dddc725ede326dbad
Em Comeau <comeau.amanda.n@gmail.com>
2021-04-21 11:54:06 -0400

    main branch protection test

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -14,10 +14,12 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
+    allowNull: false
   },
   githubId: {
     type: Sequelize.INTEGER
-  }
+  },
+  
 })
 
 module.exports = User
