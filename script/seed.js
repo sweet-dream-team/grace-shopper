596f7adaef1e9b98d0ae0b5fd358985b97744009
Kristin Carrillo <kcarrill@gmail.com>
2021-04-23 15:06:37 -0700

    edited db image file names

--- script/seed.js
+++ script/seed.js
@@ -2,7 +2,7 @@
 
 const {
   db,
-  models: { Product, User, Order},
+  models: { Product, User, Order },
 } = require("../server/db");
 
 /**
@@ -22,8 +22,7 @@ async function seed() {
         "Are you a big fan of jelly? Or beans? Well have we got a dream for people who actually like jellybeans.",
       type: "SWEET",
       duration: 7,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/3eb9d2d0fa4d20f85dc7351a930cdbd9.png",
+      imageURL: "jellybeandream.png",
       stockQuantity: 5,
     }),
     Product.create({
@@ -33,8 +32,7 @@ async function seed() {
         "This dream is so sweet you should bring your toothbrush to bed! Think of all your favorite sweets: they. are. there.",
       type: "SWEET",
       duration: 5,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/214c5930e2e3e1758cdd6ad674d73d33.png",
+      imageURL: "thesweetestdream.png",
       stockQuantity: 5,
     }),
     Product.create({
@@ -44,8 +42,7 @@ async function seed() {
         "One of the most relaxing places to be: now without sand! Enjoy a relaxing day at the beach... at night! While you sleep!",
       type: "SWEET",
       duration: 3,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/46ff1252168da0d179c01f79f2dfe786.png",
+      imageURL: "adreamatthebeach.png",
       stockQuantity: 5,
     }),
     Product.create({
@@ -55,8 +52,7 @@ async function seed() {
         "Be the ringmaster of your own dream! Dance with the elephants and fly through the air on trapeze like the star you are!",
       type: "SWEET",
       duration: 9,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/8228073375a90f5bc2894ac1fc5237af.png",
+      imageURL: "thecircusdream.png",
       stockQuantity: 5,
     }),
     Product.create({
@@ -66,8 +62,7 @@ async function seed() {
         "Time to spend them dolla dolla bills in your dreams! Own the most luxurious car or boat or plane or whatever rich people are riding in these days! Buy a 10,000 dollar sandwich just because you can!",
       type: "SWEET",
       duration: 6,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/fc6b88750874396c545c0f75b7d48477.png",
+      imageURL: "dreamillionaire.png",
       stockQuantity: 5,
     }),
     Product.create({
@@ -77,8 +72,7 @@ async function seed() {
         "Once upon a time there was a magical fairy who ran the kingdom. It was you! Battle dragons and save the prince in this epic dream!",
       type: "SWEET",
       duration: 3,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/16f3e27db0202da3cf6e36e1861d2ad5.png",
+      imageURL: "onceuponadream.png",
       stockQuantity: 5,
     }),
     Product.create({
@@ -88,8 +82,7 @@ async function seed() {
         "Oh god! You showed up to class wearing nothing but your skivvies. Get ready to deal with this all night.",
       type: "NIGHTMARE",
       duration: 8,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/b272a5e72348a56cc0136eb16d2d9133.png",
+      imageURL: "schoolinundies.png",
       stockQuantity: 10,
     }),
     Product.create({
@@ -99,8 +92,7 @@ async function seed() {
         "Dream about being in your very own horror movie? Heres your chance!",
       type: "NIGHTMARE",
       duration: 5,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/22d29e4786b1eb36d93c7dbe226001f6.png",
+      imageURL: "monsters.png",
       stockQuantity: 10,
     }),
     Product.create({
@@ -109,8 +101,7 @@ async function seed() {
       description: "AHHHHHHHHHHHHHH EVERYTHINGS ON FIRE SWEET MARY HELP US ALL",
       type: "NIGHTMARE",
       duration: 3,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/a2e45a67cab55fcbae6491f08323f2f4.png",
+      imageURL: "firenightmare.png",
       stockQuantity: 10,
     }),
     Product.create({
@@ -120,67 +111,67 @@ async function seed() {
         "We all have it. Nobody wants it. Gift someone you really dont like this disconcerting nightmare. Or have it yourself, you freak.",
       type: "NIGHTMARE",
       duration: 9,
-      imageURL:
-        "http://www.image.farm/images/2021/04/21/c37fd4fb6b07254e76e971ea939a99a1.png",
+      imageURL: "teethfallingout.png",
       stockQuantity: 10,
     }),
   ]);
 
-    //Creating Users
-    const users = await Promise.all([
+  //Creating Users
+  const users = await Promise.all([
     User.create({
       email: "michaelscott@aol.com",
       password: "thatswhatshesaid911",
-      isAdmin: true
+      isAdmin: true,
     }),
     User.create({
       email: "dwightschrute@hotmail.com",
       password: "beets123",
-      isAdmin: false
+      isAdmin: false,
     }),
     User.create({
       email: "jimhalpert@gmail.com",
       password: "ilovepam143",
-      isAdmin: false
+      isAdmin: false,
     }),
     User.create({
       email: "angelamartin@yahoo.com",
       password: "catscatscats",
-      isAdmin: false
+      isAdmin: false,
     }),
-    ])
+  ]);
 
-//Creating Orders
+  //Creating Orders
 
-const orders = await Promise.all([
-  Order.create({
-    status: 'OPEN',
-    price: 2099
-  }),
-  Order.create({
-    status: 'CONFIRMED',
-    price: 4599
-  }),
-  Order.create({
-    status: 'OPEN',
-    price: 1099
-  }),
-  Order.create({
-    status: 'CONFIRMED',
-    price: 1799
-  })
-])
+  const orders = await Promise.all([
+    Order.create({
+      status: "OPEN",
+      price: 2099,
+    }),
+    Order.create({
+      status: "CONFIRMED",
+      price: 4599,
+    }),
+    Order.create({
+      status: "OPEN",
+      price: 1099,
+    }),
+    Order.create({
+      status: "CONFIRMED",
+      price: 1799,
+    }),
+  ]);
 
-  console.log(`seeded ${products.length} dreams & ${users.length} users & ${orders.length} orders`)
-  console.log(`seeded successfully`)
+  console.log(
+    `seeded ${products.length} dreams & ${users.length} users & ${orders.length} orders`
+  );
+  console.log(`seeded successfully`);
 
-//   return {
-//     users: {
-//       cody: users[0],
-//       murphy: users[1],
-//     },
-//   };
-
+  //   return {
+  //     users: {
+  //       cody: users[0],
+  //       murphy: users[1],
+  //     },
+  //   };
 }
 
 /*
